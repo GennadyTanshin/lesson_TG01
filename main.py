c48c53f33418e7b023406d2f60c45cfbf75e5f46
GennadyTanshin <163913220+GennadyTanshin@users.noreply.github.com>
2024-07-21 19:12:46 +0300

    update

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('help'))
+async def help(message: Message):
+    await message.answer('Этот бот умеет выполнять комманды: \n /start \n /help ')
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer('Привет!')
+
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+
+
